d2b0ac9b546b0f0c193a832b93483b065abc05fd
AL0217 <c.y.lau2@lancaster.ac.uk>
2025-01-30 00:05:11 +0000

    init

---
+++ Bible Passage Generator/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace HelloWorld{
+    class Program{
+            static void Main(String[] args){
+                using (var wb = new HttpClient())
+                {
+                    string url = "https://api.biblegateway.com/2/";
+                    var data = new NameValueCollection();
+                    data["username"] = "AL0217";
+                    data["password"] = "Andylau0217*";
+
+                    wb.BaseAddress = new Uri(url);
+                    wb.DefaultRequestHeaders.Accept.Clear();
+                    
+
+                    var response = wb.UploadValues(url, "POST", data);
+                    string response_str = Encoding.UTF8.GetString(response);
+                    Console.WriteLine(response_str);
+                }
+            }
+    }
+}
